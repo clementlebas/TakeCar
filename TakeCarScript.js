89eb6de950e34d204b174d6ff64086ba6ae6ee6c
Silv3rcrowss <masteralentz@hotmail.fr>
2018-11-07 14:41:09 +0100

    Add files via upload

--- TakeCarScript.js
+++ TakeCarScript.js
@@ -1,4 +1,25 @@
 // JavaScript source code
-document.ready{
+$(document).ready(function(){
+    
+    $("h1").fadeIn('18000');
+    $('#Inscription').click(function () {
+        $('.ConnexionPage').hide();
+        $('.ConnexionPage').removeAttr("id");
+        $('.InscriptionPage').attr('id', 'overlay');
+        $('#overlay').show('3000');
+    });
+    $('.InscriptionPage #abort').click(function () {
+        $('#overlay').hide('3000', function () { $('.InscriptionPage').removeAttr("id");});
+    });
 
-}
+    $('#Connexion').click(function () {
+        $('.InscriptionPage').hide();
+        $('.InscriptionPage').removeAttr("id");
+        $('.ConnexionPage').attr('id', 'overlay');
+        $('#overlay').show('3000');
+    });
+    $('.ConnexionPage #abort').click(function () {
+        $('#overlay').hide('3000', function () { $('.ConnexionPage').removeAttr("id"); });
+    });
+
+});
